4372f748ec1c21513835ab293b842070894e0345
niteshmodi <89907820+niteshmodi@users.noreply.github.com>
2022-07-28 15:32:14 +0530

    added procfile

--- app.js
+++ app.js
@@ -103,6 +103,6 @@ app.get("/posts/:postId", function(req, res){
 
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
